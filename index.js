d7c770aba0fb7b650d63098447347c6d442adbe9
mnoumanuaar32xc <mnoumanuaar@yahoo.com>
2018-11-26 19:08:50 +0500

    Ist commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const app = express()
+const Client = require('bitcoin-core');
+//const client = new Client({ network: 'regtest' });
+// const client_c = new Client({ port: 8332 });
+// //127.0.0.1
+// const fs = require('fs');
+const client = new Client({
+    username:'bitcoinN0deAccess',
+      password:'bitc0inRPCpass',
+      port: 8332,
+  });
+//   const client = new Client({ headers: true });
+  client.getInfo().then(([body, headers]) => console.log(body, headers));
+
+  app.listen(3000,()=> console.log("Server is running at http://localhost:3000"));
+ 
