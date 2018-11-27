9e395a4b5e48bc36f82b893cc2bb229b5e926cc3
mnoumanuaar32xc <mnoumanuaar@yahoo.com>
2018-11-27 12:19:05 +0500

    gettransaction

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require ('express')
 const app = express()
 const Client = require('bitcoin-core');
-//const client = new Client({ network: 'regtest' });
-// const client_c = new Client({ port: 8332 });
-// //127.0.0.1
-// const fs = require('fs');
+
 const client = new Client({
     username:'bitcoinN0deAccess',
       password:'bitc0inRPCpass',
       port: 8332,
   });
-//   const client = new Client({ headers: true });
-//  console.log( client.getInfo())
-// app.post('/',function(req,res){
-//     // client.getInfo();
-//     res.send(client.getInfo());
-//     })
-app.post('/getinfo',function(req,res){
+//getinfo
+  app.post('/getinfo',function(req,res){
   const batch = [
     { method: 'getblockchaininfo', parameters: [] }
   ]
    client.command(batch).then((responses) => res.send(responses));
- 
-
 });
+
+
+//gettransaction
+
+app.post('/gettransaction',function(req,res){
+
+  client.getTransactionByHash('b4dd08f32be15d96b7166fd77afd18aece7480f72af6c9c7f9c5cbeb01e686fe');
+});
+
  app.listen(3000,()=> console.log("Server is running at http://localhost:3000"));
